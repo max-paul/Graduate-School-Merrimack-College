ea53dd5ab8787e5f76311987e25c9627726bbed8
Max Paul <98336946+max-paul@users.noreply.github.com>
2022-10-31 18:45:19 -0400

    updating main

--- Alg-Discrete-struct/project1/main.py
+++ Alg-Discrete-struct/project1/main.py
@@ -23,13 +23,13 @@ def main():
     # reading in the file data
     nums = readFile()
     # starting out linkedList
-    mylist = LinkedList()
+    L = LinkedList()
     # we know the list is sorted, brute set the initial linked list
     for i in reversed(nums):
-        mylist.insertBeginning(i)
+        L.insertBeginning(i)
 
     # for each item we entered into the linked list, lets check
-    mylist.printList("List Prior to operations")
+    L.printList("List Prior to operations")
 
 
     loop = True
@@ -38,7 +38,7 @@ def main():
         Continuously prompt the user to enter an integer
         """
         try:
-            user_input = eval(input("Please enter an integer :) "))
+            x = eval(input("Please enter an integer :) "))
             loop = False
         except:
             print("Please enter a string")
@@ -48,24 +48,24 @@ def main():
         For each item we passed into the txt file
         
         get the current Node, and the value of the Next,
-        if node value == user_input then remove value from LL
-        if node values != user_input then lets add this new value BUT
+        if node value == x then remove value from LL
+        if node values != x then lets add this new value BUT
         keep integrity of linked list order :)
         """
-        mylist.nextCurrent()
-        current_data = mylist.Current.Data
+        L.nextCurrent()
+        current_data = L.Current.Data
         try:
-            next_data = mylist.Current.Next.Data
+            next_data = L.Current.Next.Data
         except:
             next_data = None
 
-        if user_input == next_data:
-            print(f"Found {user_input} in the Linked List")
-            print(f"Removing Value {user_input}")
-            mylist.removeCurrentNext()
+        if x == next_data:
+            print(f"Found {x} in the Linked List")
+            print(f"Removing Value {x}")
+            L.removeCurrentNext()
 
-        if user_input > current_data and \
-                (user_input < next_data if next_data is not None else sys.maxsize):
+        if x > current_data and \
+                (x < next_data if next_data is not None else sys.maxsize):
             """
             if user input is greater than current data AND
             user input is greater than next data
@@ -75,26 +75,26 @@ def main():
             im assuming an item in our list will be less than sys.maxsize as the next value
             sys.maxsize is the maximum size a data struct may by in python: 9223372036854775807
             """
-            print(f"Did Not Find {user_input} in the Linked List")
-            print(f"Adding Value {user_input}, Between {current_data} and {next_data}")
-            mylist.insertCurrentNext(user_input)
-        elif user_input < current_data == nums[0]:
+            print(f"Did Not Find {x} in the Linked List")
+            print(f"Adding Value {x}, Between {current_data} and {next_data}")
+            L.insertCurrentNext(x)
+        elif x < current_data == nums[0]:
             """
             Handling insertion at index 0
             
             if user input is less than the current value AND 
             the current value is equal to the lowest item in our list (Index 0 as its a sorted list)
             """
-            mylist.insertBeginning(user_input)
-        elif user_input == current_data and current_data == nums[0]:
+            L.insertBeginning(x)
+        elif x == current_data and current_data == nums[0]:
             """
             if user input == current data
             and current data is equal to the lowest item in the sorted list
             lets remove the first item
             """
-            mylist.removeBeginning()
+            L.removeBeginning()
 
-    mylist.printList("List after Operations")
+    L.printList("List after Operations")
 
 if __name__ == "__main__":
     main()
