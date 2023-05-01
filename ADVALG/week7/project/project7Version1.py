7bc77ffc60e7bab0cf70f08972c2e366793799b7
max-paul <maxkpaul21@gmail.com>
2023-05-01 18:38:11 -0400

    adding the code

--- ADVALG/week7/project/project7Version1.py
+++ ADVALG/week7/project/project7Version1.py
@@ -1,5 +1,20 @@
-from datetime import datetime
 from math import log2
+
+"""
+Create a program that calls the function myFunction(x) from 0 to 9999 and applies the Hill Climbing algorithm to find the value of x that delivers the largest value for the function
+Implement a version where the initial search value x is chosen randomly between the values 1 to 9998
+You can use this function to test your program, but your program should work with any version of myFunction(x)
+def myFunction(x):
+    if (x == 0) or x < 0:
+        return 0
+    elif ((log2(x) * 7) % 17) < (x % 13):
+        return (x + log2(x)) ** 3
+    elif ((log2(x) * 5) % 23) < (x % 19):
+        return (log2(x) * 2) ** 3
+    else:
+        return (log2(x) ** 2) - x
+
+"""
 
 def myFunction(x):
     if (x == 0) or x < 0:
@@ -12,12 +27,28 @@ def myFunction(x):
         return (log2(x) ** 2) - x
 
 def hill_climbing():
+
+    """
+    A hill climbing algorithm is a type of problem-solving method that is used to find the best possible solution to a problem.
+    The algorithm starts with an initial solution and then tries to improve it by making incremental changes to it.
+    The goal is to climb up a metaphorical "hill" of possible solutions until the algorithm reaches the best solution, or the highest point on the hill.
+
+    At each step, the algorithm evaluates the current solution and its neighboring solutions to determine which one is the best. If a neighboring solution is better,
+    it becomes the new current solution and the process repeats until no further improvements can be made.
+
+    The hill climbing algorithm is often used in optimization problems, such as finding the shortest path between two points or maximizing the efficiency of a system.
+    However, it is important to note that the algorithm may not always find the absolute best solution, as it can sometimes get stuck in a local maximum instead of
+    reaching the global maximum.
+
+
+    """
+    
     # Start with x = 1
     best_x = 0
     # Evaluate function at x = 1 to get initial best value
     best_value = myFunction(best_x)
     # Loop through all values of x from 2 to 9999
-    for x in range(0, 9999,3): # skipping 3 places as we check neighbors
+    for x in range(0, 9999,3): # skipping 3 places as we check neighbors +/- 1
         # Evaluate function at current value of x
         value = myFunction(x)
         # Check the neighboring values of x
